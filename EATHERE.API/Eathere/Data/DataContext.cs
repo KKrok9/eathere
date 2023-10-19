493fd14f8003315c09a53dad9a53ddb799f37500
Kacper Krok <kacperodespero.01@wp.pl>
2023-10-19 19:11:56 +0200

    update: initial commit

---
+++ EATHERE.API/Eathere/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Eathere.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { 
+
+        }
+    }
+}
