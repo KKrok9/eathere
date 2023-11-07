4ec82b93bc344bd1af37bdc99f647b4164aabc01
Kacper Krok <kacperodespero.01@wp.pl>
2023-11-07 18:20:17 +0100

    add: static my-profile-page

---
+++ EATHERE.API/Eathere/Migrations/20231107154914_UserTableEdited.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Eathere.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserTableEdited : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BirthdayDate",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContactNumber",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BirthdayDate",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ContactNumber",
+                table: "Users");
+        }
+    }
+}
