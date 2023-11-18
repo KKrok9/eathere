2a0728eeb0118d4493b49312ffe6e1c664d3d227
Kacper Krok <kacperodespero.01@wp.pl>
2023-11-18 13:12:51 +0100

    add: possibility to update user in my profile

--- EATHERE.CLIENT/eathere-client/src/app/modules/my-profile/containers/my-profile-page/my-profile-page.component.ts
+++ EATHERE.CLIENT/eathere-client/src/app/modules/my-profile/containers/my-profile-page/my-profile-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { User } from 'src/app/models/user.model';
@@ -11,9 +12,11 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class MyProfilePageComponent {
     user!: User;
+    fg: FormGroup;
 
     isEdit: { [key: string]: boolean } = {
-        nameAndSurname: false,
+        name: false,
+        surname: false,
         email: false,
         roleInRestaurant: false,
         birthdayDate: false,
@@ -22,15 +25,39 @@ export class MyProfilePageComponent {
     };
 
     private subscription = new Subscription();
-    constructor(private router: Router, private userService: UserService) { }
+    constructor(private router: Router, private userService: UserService, private fb: FormBuilder) {
+        this.fg = this.getFg();
+    }
 
     ngOnInit(): void {
         this.getCurrentUser();
     }
 
+    private getFg(): any {
+        return this.fb.group({
+            name: "",
+            surname: "",
+            email: "",
+            isRestaurantOwner: "",
+            birthdayDate: "",
+            contactNumber: "",
+        });
+    }
+
     private getCurrentUser(): void {
         this.subscription.add(
-            this.userService.getCurrentlyLoggedUser().subscribe(response => (this.user = response))
+            this.userService.getCurrentlyLoggedUser().subscribe(response => {
+                this.user = response;
+                // Set default values in the form group based on user data
+                this.fg.setValue({
+                    name: this.user.name || '',
+                    surname: this.user.surname || '',
+                    email: this.user.email || '',
+                    isRestaurantOwner: this.user.isRestaurantOwner || false,
+                    birthdayDate: this.user.birthdayDate || '',
+                    contactNumber: this.user.contactNumber || '',
+                });
+            })
         );
     }
 
@@ -39,6 +66,22 @@ export class MyProfilePageComponent {
         console.log(fieldName, this.isEdit[fieldName]);
     }
     saveUser(): void {
+
+        const updatedUser = {
+            id: this.user.id,
+            name: this.fg.value.name,
+            surname: this.fg.value.surname,
+            email: this.fg.value.email,
+            isRestaurantOwner: this.fg.value.isRestaurantOwner,
+            birthdayDate: this.fg.value.birthdayDate,
+            contactNumber: this.fg.value.contactNumber,
+            restaurantId: this.user.restaurantId,
+            password: this.user.password
+        };
+        console.log(updatedUser);
+        this.userService.updateUser(updatedUser).subscribe(response => {
+            console.log(response);
+        })
         for (const key in this.isEdit) {
             if (this.isEdit.hasOwnProperty(key)) {
                 this.isEdit[key] = false;
--- EATHERE.CLIENT/eathere-client/src/app/modules/my-profile/my-profile.module.ts
+++ EATHERE.CLIENT/eathere-client/src/app/modules/my-profile/my-profile.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MyProfilePageComponent } from './containers/my-profile-page/my-profile-page.component';
 
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    MyProfilePageComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+    declarations: [
+        MyProfilePageComponent
+    ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule
+    ]
 })
 export class MyProfileModule { }
