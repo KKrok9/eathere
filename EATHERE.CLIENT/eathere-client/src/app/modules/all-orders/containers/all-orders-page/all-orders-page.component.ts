77c06a46ea3e9b857e1f54141f8fe81a37eceb04
Kacper Krok <kacperodespero.01@wp.pl>
2023-11-30 19:28:53 +0100

    add: select table and dish

--- EATHERE.CLIENT/eathere-client/src/app/modules/all-orders/containers/all-orders-page/all-orders-page.component.ts
+++ EATHERE.CLIENT/eathere-client/src/app/modules/all-orders/containers/all-orders-page/all-orders-page.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Dish } from 'src/app/models/dish.model';
+import { Restaurant } from 'src/app/models/restaurant.model';
+import { DishService, RestaurantService } from 'src/app/services';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
     selector: 'app-all-orders-page',
     templateUrl: './all-orders-page.component.html',
     styleUrls: ['./all-orders-page.component.scss']
 })
-export class AllOrdersPageComponent {
+export class AllOrdersPageComponent implements OnInit {
     isModalVisible: boolean = false;
+    restaurant!: Restaurant;
+    dishes: Dish[] = [];
+    private subscription = new Subscription();
+
+    constructor(
+        private restaurantService: RestaurantService,
+        private dishService: DishService,
+        private cdr: ChangeDetectorRef
+    ) {
+
+
+    }
+    ngOnInit(): void {
+        this.getRestaurant();
+    }
+
+    private getRestaurant() {
+        this.subscription.add(
+            this.restaurantService.getRestaurantOfCurrentlyLoggedUser().subscribe(
+                (response) => {
+                    this.restaurant = response;
+                    if (this.restaurant) {
+                        this.getDishes();
+                        this.cdr.detectChanges();
+                    }
+                }
+            )
+        )
+    }
+
+    private getDishes(): void {
+        this.subscription.add(
+            this.dishService.getAllDishesFromSingleRestaurant(this.restaurant.id).subscribe(
+                (response) => {
+                    this.dishes = response;
+                },
+                (error) => {
+                    console.error(error);
+                }
+            )
+        );
+    }
 
     toggleIsVisible(): void {
         this.isModalVisible = !this.isModalVisible;
--- EATHERE.CLIENT/eathere-client/src/app/shared/components/order-modal/order-modal.component.ts
+++ EATHERE.CLIENT/eathere-client/src/app/shared/components/order-modal/order-modal.component.ts
@@ -1,11 +1,45 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { Dish } from 'src/app/models/dish.model';
+import { Table } from 'src/app/models/table.model';
+import { DishService, TableService } from 'src/app/services';
 
 @Component({
     selector: 'app-order-modal',
     templateUrl: './order-modal.component.html',
     styleUrls: ['./order-modal.component.scss']
 })
-export class OrderModalComponent {
-    @Input() toggleIsModalVisible: any;
+export class OrderModalComponent implements OnInit {
 
+    @Input() restaurantId: any;
+    @Input() dishes: any;
+    tables: Table[] = [];
+    private subscription = new Subscription();
+
+    constructor(
+        private dishService: DishService,
+        private tableService: TableService,
+        private fb: FormBuilder
+    ) { }
+    ngOnInit(): void {
+        this.getTables();
+    }
+
+    private getData() {
+
+    }
+
+    private getTables(): void {
+        this.subscription.add(
+            this.tableService.getAllTablesFromRestaurant(this.restaurantId).subscribe(
+                (response) => {
+                    this.tables = response;
+                },
+                (error) => {
+                    console.log(error);
+                }
+            )
+        )
+    }
 }
